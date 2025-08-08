d469d9028ed33679b858f60ae4d3cb16475ca634
HenryZhuHR <296506195@qq.com>
2025-08-09 01:36:54 +0800

    🍔 feat: add demo and some dependencies

--- README.md
+++ README.md
@@ -1,5 +1,11 @@
 # LLM On-Device
 
+## Install
+
+```bash
+uv sync && uv pip install tensorrt-llm
+```
+
 ## License
 
 This project is licensed under the [Apache License 2.0](https://www.apache.org/licenses/LICENSE-2.0), see the [LICENSE](LICENSE) file for details.
---
+++ scripts/download.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+git lfs install && \
+git clone git@hf.co:Qwen/Qwen3-0.6B && \
+git clone git@hf.co:Qwen/Qwen3-1.7B && \
+git clone git@hf.co:Qwen/Qwen3-4B && \
+git clone git@hf.co:Qwen/Qwen3-8B && \
+git clone git@hf.co:Qwen/Qwen3-14B
+
+
+git clone https://huggingface.co/Qwen/Qwen3-1.7B
